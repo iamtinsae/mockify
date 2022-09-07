9e212db1ebf846dc4956ebe92636e6de70de5fc8
Tinsae <iamtinsae.dev@gmail.com>
2022-09-07 23:08:04 +0300

    feat(auth): custom JWT interface with username

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,4 +1,5 @@
 import { DefaultSession } from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   /**
@@ -7,6 +8,13 @@ declare module "next-auth" {
   interface Session {
     user?: {
       id: string;
+      username: string | null;
     } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    username?: string | null;
   }
 }
