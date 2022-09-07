7540e06773e4312d534822650ea0641d978a1f20
Tinsae <iamtinsae.dev@gmail.com>
2022-09-07 22:18:05 +0300

    feat: use a custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Sofia Pro", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
