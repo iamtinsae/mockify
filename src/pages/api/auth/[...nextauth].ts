1066b00eb466ad43e0aefcee7da2175bc2f411c8
Tinsae <iamtinsae.dev@gmail.com>
2022-09-07 23:09:33 +0300

    feat(auth): include username for useSession

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-// Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
@@ -12,8 +11,21 @@ export const authOptions: NextAuthOptions = {
     session({ session, token }) {
       if (session.user && token?.sub) {
         session.user.id = token.sub;
+        session.user.username = token.username || null;
       }
       return session;
+    },
+    signIn({ profile, user }) {
+      if (typeof profile.login === "string") {
+        user.username = profile.login;
+      }
+      return true;
+    },
+    jwt({ user, token }) {
+      if (typeof user?.username === "string") {
+        return { ...token, username: user.username };
+      }
+      return token;
     },
   },
   // Configure one or more authentication providers
