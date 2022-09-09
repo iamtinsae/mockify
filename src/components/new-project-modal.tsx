6a9db5c5dd08d4bc5b49ac6cc5e89cf6463e4c42
Tinsae <iamtinsae.dev@gmail.com>
2022-09-09 06:44:00 +0300

    feat(components): create project with trpc mutation

--- src/components/new-project-modal.tsx
+++ src/components/new-project-modal.tsx
@@ -1,5 +1,12 @@
-import { Fragment, useRef } from "react";
+import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { trpc } from "../utils/trpc";
+import {
+  ExclamationCircleIcon,
+  CheckCircleIcon,
+} from "@heroicons/react/24/solid";
+import cn from "classnames";
+import { useRouter } from "next/router";
 
 type NewProjectModalProps = {
   open: boolean;
@@ -11,10 +18,29 @@ export default function NewProjectModal({
   setOpen,
 }: NewProjectModalProps) {
   const cancelButtonRef = useRef(null);
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const { mutateAsync, isSuccess, isLoading, isError } =
+    trpc.useMutation("projects.create");
+
+  const handleSubmit = async () => {
+    try {
+      const project = await mutateAsync({
+        name,
+        description,
+      });
+      router.push(`/dashboard/${project.slug}`);
+    } catch {}
+  };
 
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={(open) => (!isLoading ? setOpen(open) : null)}
+      >
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -61,6 +87,8 @@ export default function NewProjectModal({
                               type="text"
                               name="name"
                               id="name"
+                              value={name}
+                              onChange={(e) => setName(e.target.value)}
                               className="shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-200"
                               placeholder="Mockify"
                               aria-describedby="name-description"
@@ -85,11 +113,22 @@ export default function NewProjectModal({
                             <textarea
                               name="description"
                               id="description"
+                              value={description}
+                              onChange={(e) => setDescription(e.target.value)}
                               className="shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-200"
                               placeholder="My awesome mocking api for use with my awesome project."
                               aria-describedby="description-description"
                             />
                           </div>
+                        </div>
+
+                        <div className="mt-4">
+                          {isError ? (
+                            <div className="text-red-600 py-2">
+                              <ExclamationCircleIcon className="h-6 inline-block mr-2" />
+                              Failed to create project. Please try again!
+                            </div>
+                          ) : null}
                         </div>
                       </div>
                     </div>
@@ -98,10 +137,42 @@ export default function NewProjectModal({
                 <div className="bg-gray-50 dark:bg-gray-700 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => setOpen(false)}
+                    className={cn(
+                      "inline-flex w-full items-center disabled:bg-gray-500 disabled:text-gray-600 disabled:cursor-not-allowed justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm transition",
+                      isSuccess
+                        ? "bg-green-600 hover:bg-green-600 cursor-default focus:ring-0 focus:ring-offset-0"
+                        : ""
+                    )}
+                    onClick={() => (isSuccess ? null : handleSubmit())}
+                    disabled={isLoading}
                   >
-                    Create Project
+                    {isLoading ? (
+                      <div>
+                        <svg
+                          className="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                        <span>Creating Project</span>
+                      </div>
+                    ) : isSuccess ? (
+                      <div className="flex items-center">
+                        <CheckCircleIcon className="w-4 h-4 mr-2 mb-1 inline-block" />
+                        <span>Redirecting...</span>
+                      </div>
+                    ) : (
+                      <span>Create Project</span>
+                    )}
                   </button>
                   <button
                     type="button"
