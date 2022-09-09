ccf6631516ea927e869f6f579e890b179798c8c8
Tinsae <iamtinsae.dev@gmail.com>
2022-09-09 10:20:55 +0300

    feat(server): create query to get authenticated user project

--- src/server/router/projects.ts
+++ src/server/router/projects.ts
@@ -2,21 +2,37 @@ import z from "zod";
 import { generateUniqueSlug } from "../../utils/slug";
 import { createProtectedRouter } from "./context";
 
-export const projectsRouter = createProtectedRouter().mutation("create", {
-  input: z.object({
-    name: z.string(),
-    description: z.string().default(""),
-  }),
-  async resolve({ ctx, input }) {
-    const slug = generateUniqueSlug(input.name);
-    const project = await ctx.prisma.project.create({
-      data: {
-        slug,
-        creatorId: ctx.session.user.id,
-        ...input,
-      },
-    });
+export const projectsRouter = createProtectedRouter()
+  .mutation("create", {
+    input: z.object({
+      name: z.string(),
+      description: z.string().default(""),
+    }),
+    async resolve({ ctx, input }) {
+      const slug = generateUniqueSlug(input.name);
+      const project = await ctx.prisma.project.create({
+        data: {
+          slug,
+          creatorId: ctx.session.user.id,
+          ...input,
+        },
+      });
 
-    return project;
-  },
-});
+      return project;
+    },
+  })
+  .query("getMyProject", {
+    input: z.object({
+      slug: z.string(),
+    }),
+    async resolve({ ctx, input: { slug } }) {
+      const project = await ctx.prisma.project.findFirst({
+        where: {
+          slug,
+          creatorId: ctx.session.user.id,
+        },
+      });
+
+      return project;
+    },
+  });
