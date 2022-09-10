9e6f02dfc993eb5895f435be3cffb2b74a8ac998
Tinsae <iamtinsae.dev@gmail.com>
2022-09-10 13:33:59 +0300

    feat(server): create delete resource mutation

--- src/server/router/resources.ts
+++ src/server/router/resources.ts
@@ -1,26 +1,52 @@
 import { z } from "zod";
 import { createProtectedRouter } from "./context";
 
-export const resourcesRoute = createProtectedRouter().mutation("create", {
-  input: z.object({
-    projectSlug: z.string(),
-    name: z.string().regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/),
-  }),
-  async resolve({ ctx, input }) {
-    const project = await ctx.prisma.project.findFirstOrThrow({
-      where: {
-        slug: input.projectSlug,
-        creatorId: ctx.session.user.id,
-      },
-    });
+export const resourcesRoute = createProtectedRouter()
+  .mutation("create", {
+    input: z.object({
+      projectSlug: z.string(),
+      name: z.string().regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/),
+    }),
+    async resolve({ ctx, input }) {
+      const project = await ctx.prisma.project.findFirstOrThrow({
+        where: {
+          slug: input.projectSlug,
+          creatorId: ctx.session.user.id,
+        },
+      });
 
-    const resource = await ctx.prisma.resource.create({
-      data: {
-        name: input.name,
-        projectId: project.id,
-      },
-    });
+      const resource = await ctx.prisma.resource.create({
+        data: {
+          name: input.name,
+          projectId: project.id,
+        },
+      });
 
-    return resource;
-  },
-});
+      return resource;
+    },
+  })
+  .mutation("delete", {
+    input: z.object({
+      resourceId: z.string(),
+    }),
+    async resolve({ ctx, input: { resourceId } }) {
+      // check if a resource exists with the resource id
+      // and creator id of requestor
+      await ctx.prisma.resource.findFirstOrThrow({
+        where: {
+          id: resourceId,
+          project: {
+            creatorId: ctx.session.user.id,
+          },
+        },
+      });
+
+      await ctx.prisma.resource.delete({
+        where: {
+          id: resourceId,
+        },
+      });
+
+      return true;
+    },
+  });
