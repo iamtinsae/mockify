61af6afd48f13002d0f17a61a8c27a2073852f91
Tinsae <iamtinsae.dev@gmail.com>
2022-09-15 17:34:17 +0300

    refactor: move validation to lib folder

---
+++ src/lib/validation.ts
@@ -0,0 +1,17 @@
+export const allowed_schema_types = [
+  "ID",
+  "NAME",
+  "ADDRESS",
+  "PHONE_NUMBER",
+  "AGE",
+  "DATE",
+  "WORD",
+] as const;
+
+export const allowed_endpoint_methods = [
+  "GET",
+  "POST",
+  "PUT",
+  "PATCH",
+  "DELETE",
+] as const;
