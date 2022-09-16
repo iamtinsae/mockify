28f39d8fe58f4ed25a206e4dcaa6143e8562b0fc
Tinsae <iamtinsae.dev@gmail.com>
2022-09-16 21:06:13 +0300

    feat(components): add option isList and listLimit

--- src/components/new-endpoint-modal.tsx
+++ src/components/new-endpoint-modal.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useRef, useState } from "react";
+import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { inferMutationInput, trpc } from "../utils/trpc";
 import {
@@ -6,18 +6,18 @@ import {
   CheckCircleIcon,
 } from "@heroicons/react/24/solid";
 import cn from "classnames";
-import {
-  allowed_endpoint_methods,
-  allowed_schema_types,
-} from "../lib/validation";
+import { allowedEndPointMethods, allowedSchemaTypes } from "../lib/validation";
 
 type NewEndPointModalProps = {
   open: boolean;
-  setOpen: (open: boolean) => void;
+  setOpen: (o: boolean) => void;
   resourceId: string;
   resourceName: string;
   afterLeave: () => void;
 };
+
+type AllowedSchemaTypes = typeof allowedSchemaTypes[number];
+type AllowedEndPointMethodsType = typeof allowedEndPointMethods[number];
 
 export default function NewEndPointModal({
   open,
@@ -26,20 +26,21 @@ export default function NewEndPointModal({
   resourceName,
   afterLeave,
 }: NewEndPointModalProps) {
-  const cancelButtonRef = useRef(null);
   const utils = trpc.useContext();
   const [name, setName] = useState("");
-  const [route, setRoute] = useState("/");
-  const [method, setMethod] =
-    useState<typeof allowed_endpoint_methods[number]>("GET");
+  const [route, setRoute] = useState("");
+  const [method, setMethod] = useState<AllowedEndPointMethodsType>("GET");
   const { mutateAsync, isSuccess, isLoading, isError, reset } =
     trpc.useMutation("endpoints.create", {
       onSuccess: () => utils.invalidateQueries("projects.getMyProject"),
     });
+
+  const [isList, setIsList] = useState(false);
+  const [listLimit, setListLimit] = useState<number | null>(null);
   const [schemas, setSchemas] = useState<
     Array<{
       name: string;
-      type: typeof allowed_schema_types[number];
+      type: AllowedSchemaTypes;
     }>
   >([]);
 
@@ -59,9 +60,26 @@ export default function NewEndPointModal({
     ]);
   };
 
+  const handleSubmit = async () => {
+    const input = {
+      name,
+      route,
+      method,
+      isList,
+      listLimit,
+      schemas,
+      resourceId,
+    };
+
+    try {
+      await mutateAsync(input);
+      setOpen(false);
+    } catch {}
+  };
+
   const renderSchemasTable = () =>
     schemas.map((schema, idx) => (
-      <tr key={`schema-${idx}`}>
+      <tr key={`schema-${idx}`} className="bg-gray-50 dark:bg-gray-900">
         <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">
           {idx + 1}
         </td>
@@ -69,7 +87,7 @@ export default function NewEndPointModal({
           <input
             type="text"
             value={schema.name}
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+            className="shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-800 dark:text-gray-200"
             onChange={(e) => {
               const schemaCopy = Array.from(schemas);
               schemaCopy.splice(idx, 1, {
@@ -85,21 +103,21 @@ export default function NewEndPointModal({
             <select
               id="location"
               name="location"
-              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              defaultValue={allowed_schema_types[0]}
+              className="mt-1 shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-800 dark:text-gray-200"
+              defaultValue={allowedSchemaTypes[0]}
               value={schema.type ?? "ID"}
               onChange={(e) => {
                 const schemaCopy = Array.from(schemas);
                 schemaCopy.splice(idx, 1, {
-                  type: e.target.value as typeof allowed_schema_types[number],
+                  type: e.target.value as AllowedSchemaTypes,
                   name: schemas[idx]?.name ?? "",
                 });
                 setSchemas(schemaCopy);
               }}
             >
-              {allowed_schema_types.map((schema_type) => (
-                <option key={schema_type} value={schema_type}>
-                  {schema_type}
+              {allowedSchemaTypes.map((schemaType) => (
+                <option key={schemaType} value={schemaType}>
+                  {schemaType}
                 </option>
               ))}
             </select>
@@ -116,19 +134,6 @@ export default function NewEndPointModal({
         </td>
       </tr>
     ));
-
-  const handleSubmit = async () => {
-    try {
-      await mutateAsync({
-        name,
-        route,
-        method,
-        schemas,
-        resourceId,
-      });
-      setOpen(false);
-    } catch {}
-  };
 
   return (
     <Transition.Root
@@ -177,9 +182,9 @@ export default function NewEndPointModal({
                     <div className="mt-3 text-center sm:mt-0 mx-4 sm:text-left">
                       <Dialog.Title
                         as="h3"
-                        className="text-lg font-medium leading-6 text-gray-900 dark:text-white"
+                        className="text-2xl mb-6 font-light leading-6 text-gray-900 dark:text-white"
                       >
-                        Create New EndPoint
+                        Create New Endpoint
                       </Dialog.Title>
                       <div className="mt-2">
                         <div>
@@ -196,9 +201,9 @@ export default function NewEndPointModal({
                               id="name"
                               value={name}
                               onChange={(e) => setName(e.target.value)}
-                              className="shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-200"
-                              placeholder="Create User"
+                              className="shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-800 dark:text-gray-200"
                               aria-describedby="name-description"
+                              autoComplete="off"
                             />
                           </div>
                         </div>
@@ -217,19 +222,17 @@ export default function NewEndPointModal({
                               id="route"
                               value={route}
                               onChange={(e) => setRoute(e.target.value)}
-                              className="shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-200"
-                              placeholder="/create/"
+                              className="shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-800 dark:text-gray-200"
                               aria-describedby="route-description"
+                              autoComplete="off"
                             />
                           </div>
                           <p
-                            className="mt-2 text-sm text-gray-500 dark:text-gray-400"
+                            className="mt-2 text-sm text-gray-500 dark:text-gray-400 bg-gray-200 dark:bg-gray-900 inline-block px-2 py-1 rounded-md"
                             id="route-description"
                           >
-                            Route can not be empty.
                             <code>
-                              /{resourceName}/
-                              {route !== "/" ? route.replace("/", "") : ""}
+                              /{resourceName}/{route}
                             </code>
                           </p>
                         </div>
@@ -237,14 +240,14 @@ export default function NewEndPointModal({
                         <div className="mt-4">
                           <label
                             htmlFor="method"
-                            className="block text-sm font-medium text-gray-700"
+                            className="block text-sm font-medium text-gray-700 dark:text-gray-200"
                           >
                             Method
                           </label>
                           <select
                             id="method"
                             name="method"
-                            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                            className="mt-1 shadow-sm focus:ring-gray-800 focus:border-gray-500 block w-full sm:text-sm border-gray-300 dark:border-gray-900 rounded-md dark:bg-gray-500 dark:bg-opacity-20 focus:dark:bg-opacity-10 text-gray-800 dark:text-gray-200"
                             defaultValue="GET"
                             onChange={(e) =>
                               setMethod(
@@ -253,12 +256,76 @@ export default function NewEndPointModal({
                               )
                             }
                           >
-                            {allowed_endpoint_methods.map((m) => (
+                            {allowedEndPointMethods.map((m) => (
                               <option key={m} value={m}>
                                 {m}
                               </option>
                             ))}
                           </select>
+                        </div>
+
+                        <div className="mt-4">
+                          <div className="relative flex items-start">
+                            <div className="flex items-center h-5">
+                              <input
+                                id="isList"
+                                aria-describedby="isList-description"
+                                name="isList"
+                                type="checkbox"
+                                className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                                checked={isList}
+                                onChange={(e) => setIsList(e.target.checked)}
+                              />
+                            </div>
+                            <div className="ml-3 text-sm">
+                              <label
+                                htmlFor="isList"
+                                className="font-medium text-gray-700 dark:text-gray-200"
+                              >
+                                List
+                              </label>
+                              <p
+                                id="isList-description"
+                                className="text-gray-500 dark:text-gray-400"
+                              >
+                                Return more than 1 object on request.
+                              </p>
+                            </div>
+                          </div>
+                          {isList ? (
+                            <div className="mt-2">
+                              <div className="border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
+                                <label
+                                  htmlFor="listLimit"
+                                  className="block text-xs font-medium text-gray-900 dark:text-gray-200"
+                                >
+                                  List Limit
+                                </label>
+                                <input
+                                  type="number"
+                                  name="listLimit"
+                                  id="listLimit"
+                                  className="block dark:bg-transparent w-full border-0 p-0 text-gray-900 dark:text-gray-100 placeholder-gray-500 dark:placeholder-gray-400 focus:ring-0 sm:text-sm"
+                                  placeholder=""
+                                  value={listLimit || ""}
+                                  onChange={(e) =>
+                                    setListLimit(Number(e.target.value))
+                                  }
+                                />
+                              </div>
+                              <p
+                                id="isList-description"
+                                className="text-gray-500 mt-2 text-xs dark:text-gray-400"
+                              >
+                                Limit the maximum number of objects that can be
+                                retrieved. Useful for testing pagination. This
+                                doesn&apos;t imply the number of objects
+                                returned per request rather the maximum number
+                                of objects that can be retrieved from the
+                                endpoint.
+                              </p>
+                            </div>
+                          ) : null}
                         </div>
 
                         <div className="mt-4">
@@ -269,39 +336,41 @@ export default function NewEndPointModal({
                           >
                             Add Schema
                           </button>
-                          <table className="min-w-full divide-y divide-gray-300">
-                            <thead className="bg-gray-50">
-                              <tr>
-                                <th
-                                  scope="col"
-                                  className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                                >
-                                  #
-                                </th>
-                                <th
-                                  scope="col"
-                                  className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                >
-                                  Name
-                                </th>
-                                <th
-                                  scope="col"
-                                  className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                >
-                                  Type
-                                </th>
-                                <th
-                                  scope="col"
-                                  className="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6"
-                                >
-                                  <span className="sr-only">Edit</span>
-                                </th>
-                              </tr>
-                            </thead>
-                            <tbody className="divide-y divide-gray-200 bg-white">
-                              {renderSchemasTable()}
-                            </tbody>
-                          </table>
+                          {schemas.length ? (
+                            <table className="min-w-full divide-y divide-gray-300 dark:divide-gray-700 border dark:border-gray-700 rounded mt-2">
+                              <thead className="bg-gray-50 rounded-t-lg dark:bg-gray-800">
+                                <tr>
+                                  <th
+                                    scope="col"
+                                    className="whitespace-nowrap py-2.5 pl-4 pr-3 text-left text-sm text-gray-900 dark:text-gray-300 font-light sm:pl-6"
+                                  >
+                                    #
+                                  </th>
+                                  <th
+                                    scope="col"
+                                    className="whitespace-nowrap px-2 py-2.5 text-left text-sm text-gray-900 dark:text-gray-300 font-light"
+                                  >
+                                    Name
+                                  </th>
+                                  <th
+                                    scope="col"
+                                    className="whitespace-nowrap px-2 py-2.5 text-left text-sm text-gray-900 dark:text-gray-300 font-light"
+                                  >
+                                    Type
+                                  </th>
+                                  <th
+                                    scope="col"
+                                    className="relative whitespace-nowrap py-2.5 pl-3 pr-4 sm:pr-6 dark:text-gray-300 font-light"
+                                  >
+                                    <span className="sr-only">Edit</span>
+                                  </th>
+                                </tr>
+                              </thead>
+                              <tbody className="divide-y divide-gray-200 dark:divide-gray-800 bg-white">
+                                {renderSchemasTable()}
+                              </tbody>
+                            </table>
+                          ) : null}
                         </div>
 
                         <div className="mt-4">
@@ -360,7 +429,6 @@ export default function NewEndPointModal({
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
                   >
                     Cancel
                   </button>
