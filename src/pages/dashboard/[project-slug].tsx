0cd11bd72cb4dc8fae5c2cb2fd638ae60629d726
Tinsae <iamtinsae.dev@gmail.com>
2022-09-23 20:57:08 +0300

    feat: create try link

--- src/pages/dashboard/[project-slug].tsx
+++ src/pages/dashboard/[project-slug].tsx
@@ -188,10 +188,14 @@ const ProjectPage: NextPage<ProjectPageProps> = (props) => {
                               </td>
                               <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                                 <a
-                                  href="#"
+                                  href={`/api/${props.slug}/${resource.name}/${
+                                    endPoint.route !== "/" ? endPoint.route : ""
+                                  }`}
                                   className="text-indigo-600 hover:text-indigo-900"
+                                  target={"_blank"}
+                                  rel="noreferrer"
                                 >
-                                  Edit
+                                  Try
                                 </a>
                               </td>
                             </tr>
